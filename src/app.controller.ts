8f235318f759f9cdead9661fdf1386f2c7cada53
Optimist GM <optimist_gm@hotmail.com>
2019-07-31 01:05:34 +0400

    UI Changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Render, Post, ValidationPipe, Body, Get, Res, InternalServerErrorException } from '@nestjs/common';
+import {
+  Controller,
+  Render,
+  Post,
+  ValidationPipe,
+  Body,
+  Get,
+  Res,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import * as _ from 'lodash';
 import { AppService } from './app.service';
 import { RegisterDto } from './dto/register.dto';
@@ -7,79 +16,87 @@ import { UserType } from './user-type.enum';
 
 @Controller()
 export class AppController {
+  // inject objects
+  constructor(private appService: AppService) {}
 
-    // inject objects
-    constructor(
-        private appService: AppService,
-    ) {}
+  // login
+  @Get('/')
+  @Render('login')
+  login() {}
 
-    // login
-    @Get('/')
-    @Render('login')
-    login() {}
+  @Post('signin')
+  async signIn(@Body(ValidationPipe) loginDto: LoginDto, @Res() res) {
+    const response = await this.appService.signIn(loginDto);
 
-    @Post('signin')
-    async signIn(@Body(ValidationPipe) loginDto: LoginDto, @Res() res) {
-        const response = await this.appService.signIn(loginDto);
-
-        if(response.statusCode === 200) {
-            if(_.isEmpty(response.data)) {
-                console.log("WRONG EMAIL OR PASSWORD");
-                res.redirect('/');
-            } else {
-                switch(response.data.type) {
-                    case UserType.ADMIN:
-                        res.redirect('/admin');
-                        break;
-                    case UserType.SHOP:
-                        res.redirect('/shop');
-                        break;
-                    case UserType.SUPPLIER:
-                        res.redirect('/supplier');
-                        break;
-                    default:
-                        res.redirect('/');
-                }
-            }
-        } else {
+    if (response.statusCode === 200) {
+      if (_.isEmpty(response.data)) {
+        console.log('WRONG EMAIL OR PASSWORD');
+        res.redirect('/');
+      } else {
+        switch (response.data.type) {
+          case UserType.ADMIN:
+            res.redirect('/admin');
+            break;
+          case UserType.SHOP:
+            res.redirect('/shop');
+            break;
+          case UserType.SUPPLIER:
+            res.redirect('/supplier');
+            break;
+          default:
             res.redirect('/');
         }
+      }
+    } else {
+      res.redirect('/');
     }
+  }
 
-    // register
-    @Get('signup')
-    @Render('register')
-    register() {}
+  // register
+  @Get('signup')
+  @Render('register')
+  register() {}
 
-    @Post('signup')
-    async signUp(@Body(ValidationPipe) registerDto: RegisterDto, @Res() res) {
-        const response = await this.appService.signUp(registerDto);
+  @Post('signup')
+  async signUp(@Body(ValidationPipe) registerDto: RegisterDto, @Res() res) {
+    const response = await this.appService.signUp(registerDto);
 
-        if(response.statusCode !== 201) {
-            res.redirect('/signup');
-        } else {
-            res.redirect('/');
-        }
+    if (response.statusCode !== 201) {
+      res.redirect('/signup');
+    } else {
+      res.redirect('/');
     }
+  }
 
-    // create admin
-    @Get('create-admin')
-    @Render('create-admin')
-    createAdminUser() {}
+  // create admin
+  @Get('create-admin')
+  @Render('create-admin')
+  createAdminUser() {}
 
-    @Post('create-admin')
-    async signUpAdmin(@Body(ValidationPipe) registerDto: RegisterDto, @Res() res) {
-        const response = await this.appService.signUp(registerDto);
+  @Post('create-admin')
+  async signUpAdmin(
+    @Body(ValidationPipe) registerDto: RegisterDto,
+    @Res() res,
+  ) {
+    const response = await this.appService.signUp(registerDto);
 
-        if(response.statusCode !== 201) {
-            res.redirect('/create-admin');
-        } else {
-            res.redirect('/');
-        }
+    if (response.statusCode !== 201) {
+      res.redirect('/create-admin');
+    } else {
+      res.redirect('/');
     }
+  }
 
-    @Get("logout")
-    logout(@Res() res) {
-        res.redirect("/");
-    }
+  @Get('about')
+  @Render('about')
+  about() {}
+
+  @Get('contact')
+  @Render('contact')
+  contact() {}
+
+  @Get('logout')
+  logout(@Res() res) {
+    res.redirect('/');
+  }
 }
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "anything",
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true
+  type: 'mysql',
+  host: '192.168.64.2',
+  port: 3306,
+  username: 'ghanim',
+  password: 'Optimist_GM9',
+  database: 'anything',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
 };
