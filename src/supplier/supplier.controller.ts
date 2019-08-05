ea4fc2ff4e00183e280b94ab62ab7cfc921b7cec
Optimist GM <optimist_gm@hotmail.com>
2019-08-05 12:59:29 +0400

    Views updated

--- src/supplier/supplier.controller.ts
+++ src/supplier/supplier.controller.ts
@@ -2,24 +2,28 @@ import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('supplier')
 export class SupplierController {
+  // home
+  @Get()
+  @Render('supplier/home')
+  home() {}
 
-    // home
-    @Get()
-    @Render("supplier/home")
-    home() {}
+  // profile
+  @Get('profile')
+  @Render('supplier/profile')
+  profile() {}
 
-    // profile
-    @Get("profile")
-    @Render("supplier/profile")
-    profile() {}
+  // products
+  @Get('products')
+  @Render('supplier/products')
+  products() {}
 
-    // products
-    @Get("products")
-    @Render("supplier/products")
-    products() {}
+  // orders
+  @Get('orders')
+  @Render('supplier/orders')
+  orders() {}
 
-    // orders
-    @Get("orders")
-    @Render("supplier/orders")
-    orders() {}
+  // contact admin
+  @Get('contact-admin')
+  @Render('supplier/contact-admin')
+  contactAdmin() {}
 }
